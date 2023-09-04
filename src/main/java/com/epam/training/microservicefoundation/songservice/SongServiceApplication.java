28d3d8805d3992e919175ccfa4cfc7a4230ffdb1
Yusuf Murodov <yusuf_murodov@epam.com>
2023-09-04 11:33:26 +0500

    Add context propogation

--- src/main/java/com/epam/training/microservicefoundation/songservice/SongServiceApplication.java
+++ src/main/java/com/epam/training/microservicefoundation/songservice/SongServiceApplication.java
@@ -3,11 +3,14 @@ package com.epam.training.microservicefoundation.songservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SongServiceApplication {
+
   public static void main(String[] args) {
+    Hooks.enableAutomaticContextPropagation();
     SpringApplication.run(SongServiceApplication.class, args);
   }
 }
